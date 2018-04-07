5a934d3af020ee7067c1897969b9d88bb386157d
汪涛 <tao@deMacBook-Pro.local>
2018-04-07 21:46:05 +0800

    初稿完成

--- 12306.py
+++ 12306.py
@@ -10,13 +10,14 @@ from json import loads
 from userInit import userName, pwd
 from info import station_names
 from time import sleep
+import re
 
 stationDict = {}
 for i in station_names.split('@')[1:]:
     stationList = i.split('|')
     stationDict[stationList[1]] = stationList[2]
 
-train_date = '2018-04-03'  # 出发世间
+train_date = '2018-04-20'  # 出发世间
 fromStation = '成都'
 toStation = '长沙'
 from_station = stationDict[fromStation]  # 出发地
@@ -38,8 +39,7 @@ imgCode_url = 'https://kyfw.12306.cn/passport/captcha/captcha-image?login_site=E
 captcha_check_url = 'https://kyfw.12306.cn/passport/captcha/captcha-check'
 webLogin_url = 'https://kyfw.12306.cn/passport/web/login'
 userLogin_url = 'https://kyfw.12306.cn/otn/login/userLogin'
-leftTicket_url = 'https://kyfw.12306.cn/otn/leftTicket/queryO?leftTicketDTO.train_date=%s&leftTicketDTO.from_station=%s&leftTicketDTO.to_station=%s&purpose_codes=ADULT' % (
-    train_date, from_station, to_station)
+leftTicket_url = 'https://kyfw.12306.cn/otn/leftTicket/queryO?leftTicketDTO.train_date=%s&leftTicketDTO.from_station=%s&leftTicketDTO.to_station=%s&purpose_codes=ADULT' % (train_date, from_station, to_station)
 checkUser_url = 'https://kyfw.12306.cn/otn/login/checkUser'
 submitOrderRequest_url = 'https://kyfw.12306.cn/otn/leftTicket/submitOrderRequest'
 
@@ -68,7 +68,6 @@ def login():
     }
     data = urllib.parse.urlencode(data).encode('utf-8')  # 转化为查询字符串
     html = opener.open(req, data=data).read().decode('utf-8')
-
     # 用户名、密码请求
     req = urllib.request.Request(webLogin_url)
     req.headers = headers
@@ -119,7 +118,7 @@ def login():
         }
         data = urllib.parse.urlencode(data).encode('utf-8')
         html = opener.open(req, data=data).read().decode('utf-8')
-        print(html)
+        # print('1001', html)
         return True
     else:
         print('登陆失败，正在重新登陆')
@@ -128,40 +127,14 @@ def login():
 
 # 余票查询
 def leftTicket():
-    req = urllib.request.Request(leftTicket_url)
+    req = urllib.request.Request(
+        leftTicket_url
+    )
     req.headers = headers
     html = opener.open(req).read().decode('utf-8')
     result = loads(html)
+    # print('1002', result)
     return result['data']['result']
-
-index = 0
-'''
-[3] = 车次
-[8] = 出发世间
-[9] = 到达世间
-[10] = 历时
-[23] = 软卧
-[28] = 硬卧
-[29] = 硬座
-'''
-for i in leftTicket():
-    tempList = i.split('|')
-    # print(tempList)
-    # for n in tempList:
-    #     print(index, n)
-    #     index += 1
-    try:
-        if tempList[23] == u'有' or int(tempList[23]) > 0:
-            print('''
-            该车次有票：
-            车次：%s
-            出发世间：%s
-            到达时间：%s
-            历时：%s
-            余票：%s
-            ''' % (tempList[3], tempList[8], tempList[9], tempList[10], tempList[23]))
-    except:
-        continue
 
 # 买票
 def buyTicket():
@@ -173,7 +146,7 @@ def buyTicket():
     }
     data = urllib.parse.urlencode(data).encode('utf-8')
     html = opener.open(req, data=data).read().decode('utf-8')
-    print(html)
+    # print('1003', html)
     
     # 下单第二个请求
     req = urllib.request.Request(submitOrderRequest_url)
@@ -190,22 +163,27 @@ def buyTicket():
     }
     data = urllib.parse.urlencode(data).encode('utf-8')
     html = opener.open(req, data=data).read().decode('utf-8')
-    print(html)
+    # print('1004', html)
 
     # 下单第三个请求
-    req = urllib.request.Request('https://kyfw.12306.cn/otn/confirmPassenger/initDc')
+    req = urllib.request.Request(
+        'https://kyfw.12306.cn/otn/confirmPassenger/initDc'
+        )
     req.headers = headers
     data = {
         '_json_att': ''
     }
     data = urllib.parse.urlencode(data).encode('utf-8')
     html = opener.open(req, data=data).read().decode('utf-8')
-    globalRepeatSubmitToken = re.findall(r"globalRepeatSubmitToken = '(.*?)'",html)[0]
-    print(globalRepeatSubmitToken)
-    print('1001', globalRepeatSubmitToken)
+    globalRepeatSubmitToken = re.findall(r"globalRepeatSubmitToken = '(.*?)'", html)[0]
+    key_check_isChange = re.findall(r"'key_check_isChange':'(.*?)'", html)[0]
+    leftTicketStr = re.findall(r"'leftTicketStr':'(.*?)'", html)[0]
+    # print('1005', globalRepeatSubmitToken)
     
     # 下单第四个请求
-    req = urllib.request.Request('https://kyfw.12306.cn/otn/confirmPassenger/getPassengerDTOs')
+    req = urllib.request.Request(
+        'https://kyfw.12306.cn/otn/confirmPassenger/getPassengerDTOs'
+        )
     req.headers = headers
     data = {
         '_json_att': '',
@@ -213,8 +191,81 @@ def buyTicket():
     }
     data = urllib.parse.urlencode(data).encode('utf-8')
     html = opener.open(req, data=data).read().decode('utf-8')
+    # print('1005', html)
+
+    # 下单第五个请求
+    req = urllib.request.Request(
+        'https://kyfw.12306.cn/otn/confirmPassenger/checkOrderInfo'
+        )
+    req.headers = headers
+    data = {
+        'cancel_flag': '2',
+        'bed_level_order_num': '000000000000000000000000000000',
+        'passengerTicketStr': '3,0,1,汪涛,1,420703199207086371,15801862464,N', # 3代表硬卧
+        'oldPassengerStr': '汪涛,1,420703199207086371,1_',
+        'tour_flag': 'dc',
+        'randCode': '',
+        'whatsSelect': '1',
+        '_json_att': '',
+        'REPEAT_SUBMIT_TOKEN': globalRepeatSubmitToken
+    }
+    data = urllib.parse.urlencode(data).encode('utf-8')
+    html = opener.open(req, data=data).read().decode('utf-8')
+    print('1006', html)
+
+    # 下单第六个请求
+    req = urllib.request.Request(
+        'https://kyfw.12306.cn/otn/confirmPassenger/confirmSingleForQueue'
+    )
+    data = {
+        'passengerTicketStr': '3,0,1,汪涛,1,420703199207086371,15801862464,N',
+        'oldPassengerStr': '汪涛,1,420703199207086371,1_',
+        'randCode': '',
+        'purpose_codes': '00',
+        'key_check_isChange': key_check_isChange,
+        'leftTicketStr': leftTicketStr,
+        'train_location': 'W2',
+        'choose_seats': '',
+        'seatDetailType': '000',
+        'whatsSelect': '1',
+        'roomType': '00',
+        'dwAll': 'N',
+        '_json_att': '',
+        'REPEAT_SUBMIT_TOKEN': globalRepeatSubmitToken
+    }
+    data = urllib.parse.urlencode(data).encode('utf-8')
+    html = opener.open(req, data=data).read().decode('utf-8')
+    print('1007', html)
 
 if __name__ == '__main__':
-    # 遗留问题：tempList 没有执行
-    leftTicket()
+    login()
+    '''
+    [3] = 车次
+    [8] = 出发世间
+    [9] = 到达世间
+    [10] = 历时
+    [23] = 软卧
+    [28] = 硬卧
+    [29] = 硬座
+    '''
+    index = 0
+    for i in leftTicket():
+        tempList = i.split('|')
+        # print(tempList)
+        # for n in tempList:
+        #     print(index, n)
+        #     index += 1
+        try:
+            if tempList[23] == u'有' or int(tempList[23]) > 0:
+                print('''
+                该车次有票：
+                车次：%s
+                出发世间：%s
+                到达时间：%s
+                历时：%s
+                余票：%s
+                ''' % (tempList[3], tempList[8], tempList[9], tempList[10], tempList[23]))
+                break
+        except:
+            continue
     buyTicket()
